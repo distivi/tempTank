86a451b37c6edac9286a1509a8ff9d59a94e5db9
Stan Dymedyuk (on Linux Ubuntu 12.04) <dymedyuk@gmail.com>
2013-06-04 04:37:59 +0300

    Add main.cpp with simple Tank class

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+class Tank
+{
+public:
+	Tank();
+	~Tank();
+
+	void shoot(int);
+	void moveToPosition(float[]);
+private:
+	float position[2];
+};
+
+Tank::Tank() {
+	cout<<"create tank";
+	position[0] = 0;
+	position[1] = 0;
+}
+
+Tank::~Tank() {
+	cout<<"Destroy tank\n";
+}
+
+void Tank::shoot(int count) {
+	int i = 0;
+	for (int i = 0; i <= count; ++i) {
+		cout << "Shoot! \n";
+	}
+}
+
+void Tank::moveToPosition(float newPosition[2]) {
+	cout<<"Tank move from position ("<<position[0]<<';'<<position[1]<<")\n";
+	position[0] = newPosition[0];
+	position[1] = newPosition[1];
+	cout<<"Tank move to position ("<<position[0]<<';'<<position[1]<<")\n";
+}
+
+int main(int argc, char **argv) {
+	cout<<"-------------------\n";	
+	Tank tank = Tank();
+	tank.shoot(3);
+	float newPosition[2] = {23.0, 25.3};
+	tank.moveToPosition(newPosition);
+	tank.shoot(5);
+
+	return 0;
+}
